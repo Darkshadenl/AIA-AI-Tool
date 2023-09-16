a3a1a19f6ed3a33d4bcae190c918faccab35dd84
Quinten M <quinten@infi.nl>
2023-09-16 15:42:30 +0200

    Remove try catch EmptyFileFilter

--- Core/src/Application/EndpointFilter/EmptyFileFilter.cs
+++ Core/src/Application/EndpointFilter/EmptyFileFilter.cs
@@ -4,26 +4,18 @@ public class EmptyFileFilter : IEndpointFilter
 {
     public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
     {
-        try
+        if (context.HttpContext.Request.ContentLength == 0)
         {
-            if (context.HttpContext.Request.ContentLength == 0)
-            {
-                context.HttpContext.Response.StatusCode = 400;
-                await context.HttpContext.Response.WriteAsync("No file received or file is empty.");
-                return null;
-            }
-            var form = context.HttpContext.Request.ReadFormAsync();
-            var file = form.Result.Files.Count > 0 ? form.Result.Files[0] : null;
-
-            if (file != null) return await next(context);
             context.HttpContext.Response.StatusCode = 400;
             await context.HttpContext.Response.WriteAsync("No file received or file is empty.");
             return null;
         }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            throw;
-        }
+        var form = context.HttpContext.Request.ReadFormAsync();
+        var file = form.Result.Files.Count > 0 ? form.Result.Files[0] : null;
+
+        if (file != null) return await next(context);
+        context.HttpContext.Response.StatusCode = 400;
+        await context.HttpContext.Response.WriteAsync("No file received or file is empty.");
+        return null;
     }
 }
