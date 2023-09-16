454a6c08105668f636d50a08ed2b054b7f23ce28
Quinten M <quinten@infi.nl>
2023-09-16 15:39:23 +0200

    Add tests emptyfilefilter

--- Core/src/Application/EndpointFilter/EmptyFileFilter.cs
+++ Core/src/Application/EndpointFilter/EmptyFileFilter.cs
@@ -12,23 +12,18 @@ public class EmptyFileFilter : IEndpointFilter
                 await context.HttpContext.Response.WriteAsync("No file received or file is empty.");
                 return null;
             }
+            var form = context.HttpContext.Request.ReadFormAsync();
+            var file = form.Result.Files.Count > 0 ? form.Result.Files[0] : null;
+
+            if (file != null) return await next(context);
+            context.HttpContext.Response.StatusCode = 400;
+            await context.HttpContext.Response.WriteAsync("No file received or file is empty.");
+            return null;
         }
         catch (Exception e)
         {
             Console.WriteLine(e);
             throw;
         }
-
-        var form = context.HttpContext.Request.ReadFormAsync();
-        var file = form.Result.Files.Count > 0 ? form.Result.Files[0] : null;
-
-        if (file == null)
-        {
-            context.HttpContext.Response.StatusCode = 400;
-            await context.HttpContext.Response.WriteAsync("No file received or file is empty.");
-            return null;
-        }
-
-        return await next(context);
     }
 }
--- TestProject/Application/EndpointFilter/EmptyFileFilterTest.cs
+++ TestProject/Application/EndpointFilter/EmptyFileFilterTest.cs
@@ -1,11 +1,14 @@
+using System.Diagnostics.CodeAnalysis;
 using System.IO.Pipelines;
 using aia_api.Application.EndpointFilter;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
 using Moq;
 
 namespace TestProject.Application.EndpointFilter;
 
 [TestFixture]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
 public class EmptyFileFilterTest
 {
     private Mock<EndpointFilterInvocationContext> _context;
@@ -19,7 +22,7 @@ public class EmptyFileFilterTest
     }
 
     [Test]
-    public async Task InvokeAsync_ShouldReturn400_WhenContentZero()
+    public async Task InvokeEmptyFileFilter_ShouldReturn400_WhenContentZero()
     {
         // Arrange
         var requestMock = new Mock<HttpRequest>();
@@ -46,6 +49,76 @@ public class EmptyFileFilterTest
         {
             Assert.That(result, Is.EqualTo(null));
             Assert.That(capturedMessage, Is.EqualTo(expectedMessage));
+        });
+    }
+
+    [Test]
+    public async Task InvokeEmptyFileFilter_ShouldReturn400_WhenFileIsNull()
+    {
+        // Arrange
+        var requestMock = new Mock<HttpRequest>();
+        var responseMock = new Mock<HttpResponse>();
+        var expectedMessage = "No file received or file is empty.";
+        var memoryStream = new MemoryStream();
+        var pipeWriter = PipeWriter.Create(memoryStream);
+        IFormCollection formCollection = new FormCollection(new Dictionary<string, StringValues>(), new FormFileCollection());
+
+
+        _context.Setup(x => x.HttpContext).Returns(new DefaultHttpContext());
+        _context.Setup(x => x.HttpContext.Request).Returns(requestMock.Object);
+        _context.Setup(x => x.HttpContext.Response).Returns(responseMock.Object);
+        requestMock.SetupGet(x => x.ContentLength).Returns(1);
+        responseMock.SetupGet(x => x.BodyWriter).Returns(pipeWriter);
+        requestMock.Setup(x => x.ReadFormAsync(It.IsAny<CancellationToken>()))
+            .Returns(Task.FromResult<IFormCollection>(formCollection));
+
+        // Act
+        var result = await new EmptyFileFilter().InvokeAsync(_context.Object, _next.Object);
+
+        // Assert
+        memoryStream.Position = 0;
+        var reader = new StreamReader(memoryStream);
+        var capturedMessage = await reader.ReadToEndAsync();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.EqualTo(null));
+            Assert.That(capturedMessage, Is.EqualTo(expectedMessage));
+        });
+    }
+
+    [Test]
+    public async Task InvokeAsync_ShouldCallNext_WhenFileIsNotNull()
+    {
+        // Arrange
+        var expectedValue = new object();
+        var nextMock = new Mock<EndpointFilterDelegate>();
+        nextMock.Setup(x => x(It.IsAny<EndpointFilterInvocationContext>()))
+            .Returns(new ValueTask<object?>(expectedValue));
+        var requestMock = new Mock<HttpRequest>();
+        var responseMock = new Mock<HttpResponse>();
+        var memoryStream = new MemoryStream();
+        var pipeWriter = PipeWriter.Create(memoryStream);
+        var fileMock = new Mock<IFormFile>();
+        fileMock.Setup(x => x.Length).Returns(1);
+        IFormCollection formCollection = new FormCollection(new Dictionary<string, StringValues>(), new FormFileCollection { fileMock.Object });
+
+        _context.Setup(x => x.HttpContext).Returns(new DefaultHttpContext());
+        _context.Setup(x => x.HttpContext.Request).Returns(requestMock.Object);
+        _context.Setup(x => x.HttpContext.Response).Returns(responseMock.Object);
+        requestMock.SetupGet(x => x.ContentLength).Returns(1);
+        responseMock.SetupGet(x => x.BodyWriter).Returns(pipeWriter);
+        requestMock.Setup(x => x.ReadFormAsync(It.IsAny<CancellationToken>()))
+            .Returns(Task.FromResult<IFormCollection>(formCollection));
+
+        // Act
+        var result = await new EmptyFileFilter().InvokeAsync(_context.Object, nextMock.Object);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.EqualTo(expectedValue));
+            nextMock.Verify(x => x(It.IsAny<EndpointFilterInvocationContext>()), Times.Once);
         });
     }
 
