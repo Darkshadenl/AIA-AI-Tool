22b6218d201df5ed612567812fd75d096d387737
smajwloo <smajw.vandeloo@student.avans.nl>
2023-09-28 11:15:56 +0200

    Setup hub

---
+++ Core/src/Hubs/FileHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.IO.Abstractions;
+using System.Text;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.IdentityModel.Tokens;
+
+namespace aia_api.src.Hubs
+{
+	public class FileHub : Hub
+	{
+        public async Task SendData(string encodedFile)
+        {
+            Console.WriteLine("File: " + encodedFile);
+
+            string fileAsBase64 = encodedFile.Split(',')[1];
+
+            await Clients.All.SendAsync("ReceiveData", fileAsBase64);
+        }
+    }
+}
+
