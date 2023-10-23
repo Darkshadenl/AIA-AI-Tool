888090e129367b5a891fcb7b4ea95f56f4d1f9b8
Quinten M <quinten@infi.nl>
2023-10-23 15:21:42 +0200

    Add CommentChecker helper class
    Added a new helper class, CommentChecker, to check if there are any comments present in the code files. It uses regex to find matches of comments in .ts files. Right now, it supports .ts (TypeScript) file extensions, with plans to extend support in future updates.

---
+++ Core/src/Application/Helpers/CommentChecker.cs
@@ -0,0 +1,50 @@
+using System.IO.Compression;
+using System.Text.RegularExpressions;
+
+namespace aia_api.Application.Helpers;
+
+public class CommentChecker
+{
+    private readonly ILogger<CommentChecker> _logger;
+
+    public CommentChecker(ILogger<CommentChecker> logger)
+    {
+        _logger = logger;
+    }
+
+    public bool CheckForComments(ZipArchiveEntry zipArchiveEntry, string fileExtension)
+    {
+        switch (fileExtension)
+        {
+            case ".ts":
+                string pattern = @"(\/\/[^\n]*|\/\*[\s\S]*?\*\/|\/\*\*[\s\S]*?\*\/)";
+                return FilesHaveComments(zipArchiveEntry, pattern);
+            default:
+                throw new ArgumentException("File extension not supported.");
+        }
+    }
+
+    private bool FilesHaveComments(ZipArchiveEntry file, string pattern)
+    {
+        var hasComments = false;
+        using var reader = new StreamReader(file.Open());
+        var fileContent = reader.ReadToEnd();
+
+        var matches = Regex.Matches(fileContent, pattern);
+        reader.Close();
+
+        if (matches.Count > 0)
+        {
+            hasComments = true;
+            _logger.LogInformation("The file contains comments.");
+
+            foreach (Match match in matches)
+            {
+                _logger.LogDebug("Found comment: {Value}", match.Value);
+            }
+        }
+        _logger.LogInformation("The file does not contain comments.");
+
+        return hasComments;
+    }
+}
