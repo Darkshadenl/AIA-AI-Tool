008c01ae9810ecf2efcf86fb3029086e11b573b2
Stef <stef@infi.nl>
2023-11-01 13:32:03 +0100

    Fix CommentManipulationHelper.cs regex & tests

--- Core/src/Application/Helpers/CommentManipulationHelper.cs
+++ Core/src/Application/Helpers/CommentManipulationHelper.cs
@@ -12,57 +12,70 @@ public class CommentManipulationHelper
         _logger = logger;
     }
 
-    public string CombineTokens(string[] tokens)
-    {
-        var stringBuilder = new StringBuilder();
-        foreach (string token in tokens)
-            stringBuilder.Append(token);
-        return stringBuilder.ToString().Trim();
-    }
-
     public string ReplaceCommentInCode(string generatedComment, string inputCode)
     {
-        //Remove old comments
-        inputCode = Regex.Replace(inputCode, @"\n((?<=\s|^)\/\/[^\n]*|\/\*[\s\S]*?\*\/|\/\*\*[\s\S]*?\*\/)", "").Trim();
-        
-        //Insert new comments
         MatchCollection commentMatches = GetComments(generatedComment, @"\[RETURN\](.*?)\[\/RETURN\]");
-        List<string> contentLines = InsertCommentsIntoCode(commentMatches, inputCode);
-        
-        return string.Join(Environment.NewLine, contentLines);
+
+        if (commentMatches.Count <= 0)
+        {
+            _logger.LogInformation("No blocks of [RETURN][/RETURN] found in the LLM response, skipping comment replacement for file.");
+            return inputCode;
+        }
+
+        string codeWithoutComments = RemoveExisingComments(commentMatches, inputCode);
+        List<string> updatedCodeLines = InsertCommentsIntoCode(commentMatches, codeWithoutComments);
+
+        return string.Join(Environment.NewLine, updatedCodeLines);
+    }
+
+    private string RemoveExisingComments(MatchCollection commentMatches, string inputCode)
+    {
+        foreach (Match match in commentMatches)
+        {
+            string comment = match.Groups[1].Value.Trim(Environment.NewLine.ToCharArray());
+            List<string> commentLines = comment.Split(Environment.NewLine).ToList();
+            string methodSignature = commentLines.Last();
+
+            string specificCommentPattern = 
+                $@"[^\S\r\n]*((?<=\s|^)\/\/[^\n]*|\/\*(?:(?!\*\/)[\s\S])*\*\/|\/\*\*(?:(?!\*\/)[\s\S])*\*\/)\n*\s*{Regex.Escape(methodSignature)}";
+            inputCode = Regex.Replace(inputCode, specificCommentPattern, methodSignature);
+        }
+
+        return inputCode;
     }
 
     private List<string> InsertCommentsIntoCode(MatchCollection commentMatches, string inputCode)
     {
-        List<string> contentLines = inputCode.Split(Environment.NewLine, StringSplitOptions.TrimEntries).ToList();
-
+        List<string> inputCodeLines = inputCode.Split(Environment.NewLine).ToList();
+        
         foreach (Match match in commentMatches)
         {
-            string comment = match.Groups[1].Value.Trim();
-            List<string> commentLines = comment.Split(Environment.NewLine, StringSplitOptions.TrimEntries).ToList();
+            string comment = match.Groups[1].Value.Trim(Environment.NewLine.ToCharArray());
+            Console.WriteLine(comment);
+            List<string> commentLines = comment.Split(Environment.NewLine).ToList();
             
-            int insertIndex = GetMethodLineIndex(commentLines, contentLines);
-            if (insertIndex < 0) return contentLines;
-            commentLines.Remove(commentLines.Last());
-            contentLines.InsertRange(insertIndex, commentLines);
+            int insertIndex = GetMethodLineIndex(commentLines, inputCodeLines);
+            if (insertIndex < 0) return inputCodeLines;
+            commentLines.Remove(commentLines.Last()); 
+            inputCodeLines.InsertRange(insertIndex, commentLines);
         }
 
-        return contentLines;
+        return inputCodeLines;
     }
 
     /// <summary>
     /// Gets the index of the line that contains the method signature.
     /// </summary>
     /// <param name="commentLines">List of lines of the newly generated comment by the LLM</param>
-    /// <param name="contentLines">List of lines of the original input code</param>
+    /// <param name="inputCodeLines">List of lines of the original input code</param>
     /// <returns>The index of the line that contains the method signature</returns>
-    private int GetMethodLineIndex(List<string> commentLines, List<string> contentLines)
+    private int GetMethodLineIndex(List<string> commentLines, List<string> inputCodeLines)
     {
-        int index = contentLines.IndexOf(commentLines.Last());
+        int index = inputCodeLines.FindIndex(line => line.Trim() == commentLines.Last().Trim());
         if (index < 0)
         {
             _logger.LogDebug("Could not find the location to place the comment in. Comment: {comment}, Code: {code}", 
-                string.Join("", commentLines), string.Join("", contentLines));
+                string.Join("", commentLines), string.Join("", inputCodeLines));
         }
 
         return index;
