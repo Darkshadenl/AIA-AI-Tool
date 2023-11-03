6dd280c50dfaff5bcafda0bf38911527f713f2c9
Stef <stef@infi.nl>
2023-11-03 16:33:47 +0100

    Fix tests

--- SignalRServer/TestProject/SignalRHubTest.cs
+++ SignalRServer/TestProject/SignalRHubTest.cs
@@ -15,6 +15,7 @@ public class SignalRHubTest
     private static string _base64;
     private static string _contentType;
     private static byte[] _byteArray;
+    private static string _clientConnectionId;
 
 
     [SetUp]
@@ -35,6 +36,7 @@ public class SignalRHubTest
         _contentType = "application/zip";
         _base64 = "UEsDBBQACAAIAPhjS1cAAAAAAAAAACoBAAAHACAAdGVzdC5jc1VUDQAHlHkmZZV5JmWUeSZldXgLAAEE9gEAAAQUAAAAXY7BDoIwEETv/Yo9QiQSTThx9A+Uu1nqEpq0pem28WD4d4WmoM5tZ152Rlg0xA4lASq8o1PHK3HU4diRca0QUiMzXFDLqDGQeAn4yMVeK7nbRbnaKVxU19CNimGIVgY1WZAZZQgjAUcD0wDhOYGNpifPgPYBnkL0NiF+2bE93Kpu0RRNBU3ZrtksvgcpG37JxUgFpwr24/y/NxVnFA6ZyyWzeANQSwcIe7qJr6QAAAAqAQAAUEsDBBQACAAIAAVsS1cAAAAAAAAAAIMAAAAHACAAdGVzdC5qc1VUDQAHuocmZbyHJmW6hyZldXgLAAEE9gEAAAQUAAAA49LXVwjJyCxWAKJEheT83NzUvBKutNK85JLM/DyF5MSc5NKcxJLU4NJcjbzS3KTUIkMdBQjDSFOhmksBCIpSS0qL8qCihgraMHlrrlouLhQTTHUUTDWtFYB2+peWFJSWWCkYGnABAFBLBwjIqlIfZwAAAIMAAABQSwMEFAAIAAgAKWJLVwAAAAAAAAAArgAAAAcAIAB0ZXN0LnB5VVQNAAcvdiZlMHYmZS92JmV1eAsAAQT2AQAABBQAAABVjbEKg0AQRPv9ikGbSGwM2ASs8gvpl8tljxx4K9zt4e9HohaZcuYxj1o83Ozr7ExgH0GpCUuArQu0ppfkQm8J8CfEG3DZl6E/kFt3J2zJYjXrUQ64njNRi6cU+wlCVW9xUYoBzOqSMGOa0DAnF5W52c/+jWOPsaMvUEsHCJloci55AAAArgAAAFBLAQIUAxQACAAIAPhjS1d7uomvpAAAACoBAAAHACAAAAAAAAAAAACkgQAAAAB0ZXN0LmNzVVQNAAeUeSZllXkmZZR5JmV1eAsAAQT2AQAABBQAAABQSwECFAMUAAgACAAFbEtXyKpSH2cAAACDAAAABwAgAAAAAAAAAAAApIH5AAAAdGVzdC5qc1VUDQAHuocmZbyHJmW6hyZldXgLAAEE9gEAAAQUAAAAUEsBAhQDFAAIAAgAKWJLV5loci55AAAArgAAAAcAIAAAAAAAAAAAAKSBtQEAAHRlc3QucHlVVA0ABy92JmUwdiZlL3YmZXV4CwABBPYBAAAEFAAAAFBLBQYAAAAAAwADAP8AAACDAgAAAAA=";
         _byteArray = Convert.FromBase64String(_base64);
+        _clientConnectionId = Guid.NewGuid().ToString();
     }
 
     [TearDown]
@@ -46,15 +48,15 @@ public class SignalRHubTest
     [Test]
     public async Task MainHub_UploadZip_Success()
     {
-        await _hub.UploadChunk(_fileName, _contentType, _base64, 0, 1);
-        _mainHubMock.Verify(_ => _.UploadChunk(_fileName, _contentType, _byteArray, It.IsAny<int>(), It.IsAny<int>()));
+        await _hub.UploadChunk(_clientConnectionId, _fileName, _contentType, _base64, 0, 1);
+        _mainHubMock.Verify(_ => _.UploadChunk(It.IsAny<string>(), _fileName, _contentType, _byteArray, It.IsAny<int>(), It.IsAny<int>()));
     }
 
     [Test]
     public async Task MainHub_UploadZip_Fail_Empty_Params()
     {
-        await _hub.UploadChunk("", "", "", 0, 1);
-        _mainHubMock.Verify(_ => _.ReceiveError("No file received or file is empty."));
+        await _hub.UploadChunk(_clientConnectionId, "", "", "", 0, 1);
+        _mainHubMock.Verify(_ => _.ReceiveError(_clientConnectionId, "No file received or file is empty."));
     }
 
     [Test]
@@ -62,7 +64,7 @@ public class SignalRHubTest
     {
         string unexpectedDataUri = "Unexpected";
 
-        await _hub.UploadChunk(_fileName, _contentType, unexpectedDataUri, 0, 1);
-        _mainHubMock.Verify(_ => _.ReceiveError("The file is not converted to base64 correctly."));
+        await _hub.UploadChunk(_clientConnectionId, _fileName, _contentType, unexpectedDataUri, 0, 1);
+        _mainHubMock.Verify(_ => _.ReceiveError(_clientConnectionId, "The file is not converted to base64 correctly."));
     }
 }
