86aaa65939efb3aa315a067ad661156ee5b9eb04
Stef <stef@infi.nl>
2023-11-03 16:10:33 +0100

    Fix existing tests

--- FrontEnd/tests/test.js
+++ FrontEnd/tests/test.js
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'File upload' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'ZIP File upload' })).toBeVisible();
 });
 
 test('index page has upload button', async ({ page }) => {
