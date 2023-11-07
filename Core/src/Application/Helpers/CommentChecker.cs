75b316ce571139b2831de9378d1ad4b18ff94d4a
Stef <stef@infi.nl>
2023-11-07 11:03:51 +0100

    Fix extensionCount such that it is reset every time a new zip file is uploaded & Fix error thrown when file was not supported which resulted in a broken API

--- Core/src/Application/Helpers/CommentChecker.cs
+++ Core/src/Application/Helpers/CommentChecker.cs
@@ -39,7 +39,8 @@ public class CommentChecker
                 return FileHasComments(detectCommentsPattern, detectEslintCommentsPattern,
                     detectInlineCommentsPattern);
             default:
-                throw new ArgumentException("File extension not supported.");
+                _logger.LogDebug("File extension {extension} not supported.", fileExtension);
+                return false;
         }
     }
 
