7baff7de88ff88dcd7e224bc0743d3e9d736db7a
Quinten M <quinten@infi.nl>
2023-11-22 14:20:49 +0100

    "Refactor imports and add Diff handling to lib folder"
    Refactored the structure of imports in index.js to make it more organized. Added new file `SetupDiffData.js` which includes the class `DiffItem` and function `SetupDiffDataStructure` for handling differences between old and new file content. These changes provide a more efficient and structured way to manage and utilize the diff data in the application.

--- FrontEnd/src/lib/index.js
+++ FrontEnd/src/lib/index.js
@@ -1,12 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
-import {
-    resetStores
-} from "./store/storeManagement.js";
-
-
 export * from "./helpers.js";
 export * from './SignalRServer.js';
-
-export {
-    resetStores,
-};
+export {SetupDiffDataStructure, DiffItem} from "./SetupDiffData.js";
+export {resetStores} from "./store/storeManagement.js";
