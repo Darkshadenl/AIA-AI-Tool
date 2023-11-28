fbeea50ad6aa70d748841aae65483c1bb4d784a8
Stef <stef@infi.nl>
2023-11-28 11:03:31 +0100

    In the previous version, all comments in a file were being split into either inline comments or not inline comments. This approach was refactored to simply detect all comments without dividing them into categories. The aim of this change is to make the comment checker more efficient and reduce code complexity. Inline and not inline comments were being used in similar checks misleadingly implying different business logic when it was not the case. Now, all comments are processed in a uniform way.

--- Core/src/Application/Helpers/CommentChecker.cs
+++ Core/src/Application/Helpers/CommentChecker.cs
@@ -8,9 +8,8 @@ public class CommentChecker
     private readonly ILogger<CommentChecker> _logger;
     private readonly List<string> _logs;
     private ZipArchiveEntry _file;
-    private MatchCollection _notInlineComments;
+    private MatchCollection _allComments;
     private MatchCollection _eslintComments;
-    private MatchCollection _inlineComments;
 
     public CommentChecker(ILogger<CommentChecker> logger)
     {
@@ -32,26 +31,21 @@ public class CommentChecker
         {
             case ".ts":
                 var detectCommentsPattern = @"((?<=\s|^)\/\/[^\n]*|\/\*[\s\S]*?\*\/|\/\*\*[\s\S]*?\*\/)";
-                var detectEslintCommentsPattern =
+                var detectEslintCommentsPattern = 
                     @"(\/\/.*eslint-.*|\/\*[\s\S]*?eslint-[\s\S]*?\*\/|\/\*\*[\s\S]*?eslint-[\s\S]*?\*\/)";
-                var detectInlineCommentsPattern =
-                    @"(?<!\S)\/\/[^\n]*|(?<=\S{1,3}\s{0,2})\/\*[\s\S]*?\*\/|(?<=\S{1,3}\s{0,2})\/\*\*[\s\S]*?\*\/";
-                return FileHasComments(detectCommentsPattern, detectEslintCommentsPattern,
-                    detectInlineCommentsPattern);
+                return FileHasComments(detectCommentsPattern, detectEslintCommentsPattern);
             default:
                 _logger.LogDebug("File extension {extension} not supported.", fileExtension);
                 return false;
         }
     }
 
-    private bool FileHasComments(string allCommentPattern, string eslintCommentPattern,
-        string detectInlineCommentPattern)
+    private bool FileHasComments(string allCommentPattern, string eslintCommentPattern)
     {
         var fileContent = ReadFileContent(_file);
-        _notInlineComments = FindMatches(fileContent, allCommentPattern);
+        _allComments = FindMatches(fileContent, allCommentPattern);
         _eslintComments = FindMatches(fileContent, eslintCommentPattern);
-        _inlineComments = FindMatches(fileContent, detectInlineCommentPattern);
-
+        
         return AnalyzeComments();
     }
 
@@ -68,32 +62,24 @@ public class CommentChecker
 
     private bool AnalyzeComments()
     {
-        if (_notInlineComments.Count + _inlineComments.Count == 0)
+        if (_allComments.Count == 0)
         {
             Log($"{_file.Name} does not contain comments.");
             return false;
         }
-
-        var nonEssentialCommentCount = _eslintComments.Count;
-
-        if (nonEssentialCommentCount == _notInlineComments.Count + _inlineComments.Count)
-        {
-            Log($"{_file.Name} contains only eslint. Skipping.");
-            return false;
-        }
-
+        
         if (IsOnlyTypeOfComment(_eslintComments, "eslint"))
             return false;
 
-        LogComments(_notInlineComments);
+        LogComments(_allComments);
         return true;
     }
 
     private bool IsOnlyTypeOfComment(MatchCollection specificComments, string commentType)
     {
-        if (_notInlineComments.Count + _inlineComments.Count <= specificComments.Count)
+        if (_allComments.Count <= specificComments.Count)
         {
-            Log($"Found only {commentType} comment in {_file.FullName}. Skipping.");
+            Log($"Found only {commentType} comments in {_file.FullName}. Skipping.");
             return true;
         }
 
