2e8a868ea4119b8075751e4f1215a51dba07866d
Stef <stef@infi.nl>
2023-12-04 09:00:08 +0100

    Place zip file upload input in middle of screen

--- FrontEnd/src/routes/+layout.svelte
+++ FrontEnd/src/routes/+layout.svelte
@@ -1,25 +1,26 @@
 <script>
   import '../app.css';
-  import logo from '$lib/assets/logo.jpeg'
+  import logo from '$lib/assets/logo.jpeg';
 </script>
 
-<header class="bg-white m-5">
-  <nav class="flex items-center" aria-label="Global">
-    <div class="flex ">
-      <a href="/" class="-m-1.5 p-1.5">
-        <span class="sr-only">AIA</span>
-        <img class="h-20 w-auto" src="{logo}" alt="">
-      </a>
-    </div>
-    <div class="ml-5">
-      <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Upload File</a>
-    </div>
-    <div class="ml-5">
-      <a href="/differences" class="text-sm font-semibold leading-6 text-gray-900">Differences</a>
-    </div>
-  </nav>
-</header>
+<div class="flex flex-col h-screen">
+  <header class="bg-white m-5">
+    <nav class="flex items-center">
+      <div class="flex">
+        <a href="/" class="-m-1.5 p-1.5">
+          <img class="h-20 w-auto" src="{logo}" alt="">
+        </a>
+      </div>
+      <div class="ml-5">
+        <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Upload File</a>
+      </div>
+      <div class="ml-5">
+        <a href="/differences" class="text-sm font-semibold leading-6 text-gray-900">Differences</a>
+      </div>
+    </nav>
+  </header>
 
-<body class="m-5">
-<slot />
-</body>
+  <div class="flex flex-grow justify-center items-center">
+    <slot />
+  </div>
+</div>
